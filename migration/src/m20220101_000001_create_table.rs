85996ac57a53d33f49accd42ea5f6a0ef3623c61
Remi Petitjean <remi.petitjean@gmail.com>
2023-11-14 22:50:59 +0000

    exchange

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -22,10 +22,36 @@ impl MigrationTrait for Migration {
                     .col(ColumnDef::new(Exchange::Timezone).string().not_null())
                     .to_owned(),
             )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Stock::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Stock::Id).string().not_null().primary_key())
+                    .col(ColumnDef::new(Stock::Name).string().not_null())
+                    .col(ColumnDef::new(Stock::Currency).string().not_null())
+                    .col(ColumnDef::new(Stock::ExchangeId).string().not_null())
+                    .col(ColumnDef::new(Stock::MicCode).string().not_null())
+                    .col(ColumnDef::new(Stock::Country).string().not_null())
+                    .col(ColumnDef::new(Stock::Type).string().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_stock_exchange_id")
+                            .from(Stock::Table, Stock::ExchangeId)
+                            .to(Exchange::Table, Exchange::Id),
+                    )
+                    .to_owned(),
+            )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Stock::Table).to_owned())
+            .await?;
+
         manager
             .drop_table(Table::drop().table(Exchange::Table).to_owned())
             .await
@@ -39,4 +65,16 @@ enum Exchange {
     Name,
     Country,
     Timezone,
+}
+
+#[derive(DeriveIden)]
+enum Stock {
+    Table,
+    Id,
+    Name,
+    Currency,
+    ExchangeId,
+    MicCode,
+    Country,
+    Type,
 }
